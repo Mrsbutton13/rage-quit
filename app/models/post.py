b0ae4fff951070a6ae1b8ebca7e9c39e4aefecfb
laura button <llalla02@msn.com>
2021-06-18 03:22:51 -0600

    user is able to post image

--- app/models/post.py
+++ app/models/post.py
@@ -7,16 +7,22 @@ class Post(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   body = db.Column(db.Text)
+  post_img = db.Column(db.Text)
+  post_video = db.Column(db.Text)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
   created_on = db.Column(db.DateTime, server_default=func.now())
 
-  comments = db.relationship('PostComment')
+  comments = db.relationship(
+    'PostComment',
+    cascade = 'all, delete, delete-orphan')
 
 
   def to_dict(self):
     return {
     'id': self.id,
     'body': self.body,
+    'post_img': self.post_img,
+    'post_video': self.post_video,
     'user_id': self.user_id,
     'created_on': self.created_on,
     'comments': [comment.id for comment in self.comments]
